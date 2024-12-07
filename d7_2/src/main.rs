fc1bbda542c99da7db879545c2a3ac91abd89063
kimmk <kimmo.korpelin@gmail.com>
2024-12-07 21:34:21 +0200

    d7p2

---
+++ d7_2/src/main.rs
@@ -0,0 +1,44 @@
+
+fn calc_ops(
+    nums: &Vec<i64>,
+    result: i64
+) -> bool {
+    let mut ret = false;
+    let sum = nums[0] + nums[1];
+    let pro = nums[0] * nums[1];
+    let cat = format!("{}{}", nums[0], nums[1]).parse::<i64>().unwrap();
+    if nums.len() == 2 {
+        ret |= sum == result;
+        ret |= pro == result;
+        ret |= cat == result;
+    }
+    else {
+        let nums_rest = &nums[2..];
+        let mut sum_nums = vec![sum];
+        sum_nums.extend_from_slice(nums_rest);
+        ret |= calc_ops(&sum_nums, result);
+        let mut pro_nums = vec![pro];
+        pro_nums.extend_from_slice(nums_rest);
+        ret |= calc_ops(&pro_nums, result);
+        let mut cat_nums = vec![cat];
+        cat_nums.extend_from_slice(nums_rest);
+        ret |= calc_ops(&cat_nums, result);
+    }
+    return ret;
+}
+
+fn main() {
+    let input = include_str!("input");
+    let mut total_calib = 0;
+    for input_line in input.lines() {
+        let (result_str, operands_str) = input_line.split_once(": ").unwrap();
+        let result = result_str.parse::<i64>().unwrap();
+        let operands = operands_str.split(" ").map(|op| op.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        println!("operands: {:?}", operands);
+        if calc_ops(&operands, result) {
+            println!("result found");
+            total_calib += result;
+        }
+    }
+    println!("total calib: {}", total_calib);
+}
